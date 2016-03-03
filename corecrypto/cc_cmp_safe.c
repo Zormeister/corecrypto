584290899ac0ad95aa90c21622c4ca77a856afa1
William Kent <wjk@sunsol.me>
2016-03-02 19:44:44 -0500

    Add cc_cmp_safe() function

---
+++ corecrypto/cc_cmp_safe.c
@@ -0,0 +1,13 @@
+#include <corecrypto/cc.h>
+
+int cc_cmp_safe(size_t num, const void *ptr1, const void *ptr2) {
+	if (num == 0) return 1;
+
+	volatile const unsigned char *buffer1 = ptr1;
+	volatile const unsigned char *buffer2 = ptr2;
+	for (size_t i = 0; i < num; i++) {
+		if (buffer1[i] != buffer2[i]) return 1;
+	}
+
+	return 0;
+}
