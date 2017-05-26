67d1f3170271dc3a34da9f2425a068ef436fdcd0
William Kent <wjk@sunsol.me>
2017-05-26 16:23:59 -0400

    Revert "Register the mbedTLS callbacks on kext load"
    These changes are incompatible with any version of xnu currently
    extant (including the old one in Andromeda-OS/Kernel). Therefore,
    they are of no use to anyone anymore, myself included.
    
    This reverts commit 14d697131898a0f40de4720562bb68632045e7b6.

--- corecrypto/kext/cc_kext_main.c
+++ corecrypto/kext/cc_kext_main.c
@@ -1,24 +1,11 @@
 #include <mach/mach_types.h>
-#include <kern/debug.h>
-#include <libkern/crypto/register_crypto.h>
-#include <mbedtls/aes.h>
-
-static struct mbedtls_functions cc_mbedtls_functions = {
-	mbedtls_aes_init,
-	mbedtls_aes_free,
-	mbedtls_aes_setkey_enc,
-	mbedtls_aes_setkey_dec,
-	mbedtls_aes_crypt_cbc
-};
 
 kern_return_t cc_kext_start(kmod_info_t * ki, void *d)
 {
-	int success = register_mbedtls_callbacks(&cc_mbedtls_functions);
-	if (success != 0) panic("Could not register mbedTLS callbacks");
-	return KERN_SUCCESS;
+    return KERN_SUCCESS;
 }
 
 kern_return_t cc_kext_stop(kmod_info_t *ki, void *d)
 {
-	return KERN_FAILURE;
+    return KERN_FAILURE;
 }
