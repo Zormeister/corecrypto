dbe0e3497532cab0542b46d9a50ad7d07992065d
William Kent <wjk@sunsol.me>
2017-07-25 19:32:26 -0400

    Implement cc_kext_start()

--- src/kext/cc_kext_main.c
+++ src/kext/cc_kext_main.c
@@ -1,7 +1,18 @@
+#include <sys/systm.h>
 #include <mach/mach_types.h>
+#include "register_crypto.h"
+
+extern struct crypto_functions pdcrypto_internal_functions;
 
 kern_return_t cc_kext_start(kmod_info_t * ki, void *d)
 {
+	int ret = register_crypto_functions(&pdcrypto_internal_functions);
+	if (ret == -1) {
+		printf("warning: corecrypto could not be registered. Did another crypto handler beat us to it?\n");
+	} else {
+		printf("CC loaded\n");
+	}
+
     return KERN_SUCCESS;
 }
 
