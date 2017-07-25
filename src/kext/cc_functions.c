07101c5aeaf65f2b283eb939151aa83cb7911584
William Kent <wjk@sunsol.me>
2017-07-25 19:30:31 -0400

    Fill out the crypto_functions table

--- src/kext/cc_functions.c
+++ src/kext/cc_functions.c
@@ -2,103 +2,59 @@
 #include <corecrypto/cc_abort.h>
 #include <corecrypto/ccmd5.h>
 #include <corecrypto/ccsha1.h>
-
-void cchmac_init_stub(const struct ccdigest_info *di, cchmac_ctx_t ctx, unsigned long key_len, const void *key) {
-	cc_abort("cchmac_init(): function unimplemented");
-}
-
-void cchmac_update_stub(const struct ccdigest_info *di, cchmac_ctx_t ctx, unsigned long data_len, const void *data) {
-	cc_abort("cchmac_update(): function unimplemented");
-}
-
-void cchmac_final_stub(const struct ccdigest_info *di, cchmac_ctx_t ctx, unsigned char *mac) {
-	cc_abort("cchmac_final(): function unimplemented");
-}
-
-void cchmac_stub(const struct ccdigest_info *di, unsigned long key_len, const void *key, unsigned long data_len, const void *data, unsigned char *mac) {
-	cc_abort("cchmac(): function unimplemented");
-}
-
-void ccpbkdf2_hmac_stub(const struct ccdigest_info *di, unsigned long passwordLen, const void *password, unsigned long saltLen, const void *salt, unsigned long iterations, unsigned long dkLen, void *dk) {
-	cc_abort("ccpbkdf2_hmac(): function unimplemented");
-}
-
-void ccdes_key_is_weak_stub(void *key, unsigned long length) {
-	cc_abort("ccdes_key_is_weak(): function unimplemented");
-}
-
-void ccdes_key_set_odd_parity_stub(void *key, unsigned long length) {
-	cc_abort("ccdes_key_set_odd_parity(): function unimplemented");
-}
-
-void ccpad_xts_decrypt_stub(const struct ccmode_xts *xts, ccxts_ctx *ctx, unsigned long nbytes, const void *in, void *out) {
-	cc_abort("ccpad_xts_decrypt(): function unimplemented");
-}
-
-void ccpad_xts_encrypt_stub(const struct ccmode_xts *xts, ccxts_ctx *ctx, unsigned long nbytes, const void *in, void *out) {
-	cc_abort("ccpad_xts_encrypt(): function unimplemented");
-}
+#include "../algorithms/pdcrypto_dummy.h"
 
 extern void pdcdigest_init(const struct ccdigest_info *di, ccdigest_ctx_t ctx);
 extern void pdcdigest_update(const struct ccdigest_info *di, ccdigest_ctx_t ctx, unsigned long len, const void *data);
 extern void pdcdigest_final_fn(const struct ccdigest_info *di, ccdigest_ctx_t ctx, void *digest);
 extern void pdcdigest_fn(const struct ccdigest_info *di, unsigned long len, const void *data, void *digest);
 
-#if 0
-const struct crypto_functions _cc_callbacks = {
-	/* digests common functions */
-	pdcdigest_init,
-	pdcdigest_update,
-	pdcdigest_final_fn,
-	pdcdigest_fn,
+extern const struct ccdigest_info pdcmd5_di;
+extern const struct ccdigest_info pdcsha1_di;
+extern const struct ccmode_ecb pdcaes_ecb_encrypt;
+extern const struct ccmode_ecb pdcaes_ecb_decrypt;
+extern const struct ccmode_cbc pdcaes_cbc_encrypt;
+extern const struct ccmode_cbc pdcaes_cbc_decrypt;
 
-	/* digest implementations */
-	&ccmd5_ltc_di,
-	&ccsha1_ltc_di,
-	//const struct ccdigest_info * ccsha256_di;
-	//const struct ccdigest_info * ccsha384_di;
-	//const struct ccdigest_info * ccsha512_di;
+const struct crypto_functions pdcrypto_internal_functions = {
+	.ccdigest_init_fn = pdcdigest_init,
+	.ccdigest_update_fn = pdcdigest_update,
+	.ccmd5_di = &pdcmd5_di,
+	.ccsha1_di = &pdcsha1_di,
 
-	/* hmac common function */
-	cchmac_init_stub,
-	cchmac_update_stub,
-	cchmac_final_stub,
-	cchmac_stub,
-
-	/* ciphers modes implementations */
-	/* AES, ecb, cbc and xts */
-	//const struct ccmode_ecb *ccaes_ecb_encrypt;
-	//const struct ccmode_ecb *ccaes_ecb_decrypt;
-	//const struct ccmode_cbc *ccaes_cbc_encrypt;
-	//const struct ccmode_cbc *ccaes_cbc_decrypt;
-	//const struct ccmode_xts *ccaes_xts_encrypt;
-	//const struct ccmode_xts *ccaes_xts_decrypt;
-	//const struct ccmode_gcm *ccaes_gcm_encrypt;
-	//const struct ccmode_gcm *ccaes_gcm_decrypt;
-	/* DES, ecb and cbc */
-	//const struct ccmode_ecb *ccdes_ecb_encrypt;
-	//const struct ccmode_ecb *ccdes_ecb_decrypt;
-	//const struct ccmode_cbc *ccdes_cbc_encrypt;
-	//const struct ccmode_cbc *ccdes_cbc_decrypt;
-	/* Triple DES, ecb and cbc */
-	//const struct ccmode_ecb *cctdes_ecb_encrypt;
-	//const struct ccmode_ecb *cctdes_ecb_decrypt;
-	//const struct ccmode_cbc *cctdes_cbc_encrypt;
-	//const struct ccmode_cbc *cctdes_cbc_decrypt;
-	/* RC4 */
-	//const struct ccrc4_info *ccrc4_info;
-	/* Blowfish - ECB only */
-	//const struct ccmode_ecb *ccblowfish_ecb_encrypt;
-	//const struct ccmode_ecb *ccblowfish_ecb_decrypt;
-	/* CAST - ECB only */
-	//const struct ccmode_ecb *cccast_ecb_encrypt;
-	//const struct ccmode_ecb *cccast_ecb_decrypt;
-	/* DES key helper functions */
-	ccdes_key_is_weak_stub,
-	ccdes_key_set_odd_parity_stub,
-
-	/* XTS padding functions */
-	ccpad_xts_encrypt_stub,
-	ccpad_xts_decrypt_stub
+	.ccdigest_final_fn = pdcdigest_final_fn,
+	.ccdigest_fn = pdcdigest_fn,
+	.ccsha256_di = &pdcsha256_di_dummy,
+	.ccsha384_di = &pdcsha384_di_dummy,
+	.ccsha512_di = &pdcsha512_di_dummy,
+	.cchmac_init_fn = pdchmac_init_fn_dummy,
+	.cchmac_update_fn = pdchmac_update_fn_dummy,
+	.cchmac_final_fn = pdchmac_final_fn_dummy,
+	.cchmac_fn = pdchmac_fn_dummy,
+	.ccaes_ecb_encrypt = &pdcaes_ecb_encrypt,
+	.ccaes_ecb_decrypt = &pdcaes_ecb_decrypt,
+	.ccaes_cbc_encrypt = &pdcaes_cbc_encrypt,
+	.ccaes_cbc_decrypt = &pdcaes_cbc_decrypt,
+	.ccaes_xts_encrypt = &pdcaes_xts_encrypt_dummy,
+	.ccaes_xts_decrypt = &pdcaes_xts_decrypt_dummy,
+	.ccaes_gcm_encrypt = &pdcaes_gcm_encrypt_dummy,
+	.ccaes_gcm_decrypt = &pdcaes_gcm_decrypt_dummy,
+	.ccdes_ecb_encrypt = &pdcdes_ecb_encrypt_dummy,
+	.ccdes_ecb_decrypt = &pdcdes_ecb_decrypt_dummy,
+	.ccdes_cbc_encrypt = &pdcdes_cbc_encrypt_dummy,
+	.ccdes_cbc_decrypt = &pdcdes_cbc_decrypt_dummy,
+	.cctdes_ecb_encrypt = &pdctdes_ecb_encrypt_dummy,
+	.cctdes_ecb_decrypt = &pdctdes_ecb_decrypt_dummy,
+	.cctdes_cbc_encrypt = &pdctdes_cbc_encrypt_dummy,
+	.cctdes_cbc_decrypt = &pdctdes_cbc_decrypt_dummy,
+	.ccrc4_info = &pdcrc4_info_dummy,
+	.ccblowfish_ecb_encrypt = &pdcblowfish_ecb_encrypt_dummy,
+	.ccblowfish_ecb_decrypt = &pdcblowfish_ecb_decrypt_dummy,
+	.cccast_ecb_encrypt = &pdccast_ecb_encrypt_dummy,
+	.cccast_ecb_decrypt = &pdccast_ecb_decrypt_dummy,
+	.ccdes_key_is_weak_fn = pdcdes_key_is_weak_fn_dummy,
+	.ccdes_key_set_odd_parity_fn = pdcdes_key_set_odd_parity_fn_dummy,
+	.ccpad_xts_encrypt_fn = pdcpad_xts_encrypt_fn_dummy,
+	.ccpad_xts_decrypt_fn = pdcpad_xts_decrypt_fn_dummy
 };
-#endif
+
