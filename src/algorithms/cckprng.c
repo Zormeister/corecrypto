1dee7ec8f30b2436fd55850ab88f3c80c4d80045
William Kent <wjk011@gmail.com>
2019-10-14 15:17:12 -0400

    Use all eight bytes of rdrand output

--- src/algorithms/cckprng.c
+++ src/algorithms/cckprng.c
@@ -47,17 +47,21 @@ int cckprng_generate(cckprng_ctx_t ctx, size_t nbytes, void *out) {
 	uint8_t *buffer = (uint8_t *)out;
 
 	for (size_t i = 0; i < nbytes; i++) {
-		int mod4 = i % 4;
-		if (mod4 == 0) {
+		int mod8 = i % 8;
+		if (mod8 == 0) {
 			int error = rdrand(&random);
 			if (error != CCKPRNG_OK) return error;
 		}
 
 		uint8_t byte;
-		if (mod4 == 0) byte = random & 0xFF;
-		else if (mod4 == 1) byte = (random >> 8) & 0xFF;
-		else if (mod4 == 2) byte = (random >> 16) & 0xFF;
-		else if (mod4 == 3) byte = (random >> 24) & 0xFF;
+		if (mod8 == 0) byte = random & 0xFF;
+		else if (mod8 == 1) byte = (random >> 8) & 0xFF;
+		else if (mod8 == 2) byte = (random >> 16) & 0xFF;
+		else if (mod8 == 3) byte = (random >> 24) & 0xFF;
+		else if (mod8 == 4) byte = (random >> 32) & 0xFF;
+		else if (mod8 == 5) byte = (random >> 40) & 0xFF;
+		else if (mod8 == 6) byte = (random >> 48) & 0xFF;
+		else if (mod8 == 7) byte = (random >> 56) & 0xFF;
 		else return CCKPRNG_ABORT;
 
 		buffer[i] = byte;
