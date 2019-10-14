581e43559946c0ff8f480dabfee38e9ab10cd291
William Kent <wjk011@gmail.com>
2019-10-14 14:26:06 -0400

    Call register_and_init_prng in cc_kext_start

--- src/kext/cc_kext_main.c
+++ src/kext/cc_kext_main.c
@@ -1,6 +1,8 @@
 #include <sys/systm.h>
 #include <mach/mach_types.h>
 #include "register_crypto.h"
+#include "prng_random.h"
+#include "../algorithms/cckprng.h"
 
 extern struct crypto_functions pdcrypto_internal_functions;
 
@@ -9,10 +11,16 @@ kern_return_t cc_kext_start(kmod_info_t * ki, void *d)
 	int ret = register_crypto_functions(&pdcrypto_internal_functions);
 	if (ret == -1) {
 		printf("warning: corecrypto could not be registered. Did another crypto handler beat us to it?\n");
-	} else {
-		printf("corecrypto loaded\n");
 	}
 
+	struct prng_fns fns;
+	fns.init = cckprng_init;
+	fns.reseed = cckprng_reseed;
+	fns.addentropy = cckprng_addentropy;
+	fns.generate = cckprng_generate;
+	register_and_init_prng(&fns);
+
+	printf("corecrypto loaded\n");
     return KERN_SUCCESS;
 }
 
