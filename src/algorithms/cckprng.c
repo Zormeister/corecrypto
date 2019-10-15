2702dbacefb961085367e6bc0c0f147dd6e7ca07
William Kent <wjk011@gmail.com>
2019-10-14 20:07:26 -0400

    Reimplement cckprng functions on top of yarrow

--- src/algorithms/cckprng.c
+++ src/algorithms/cckprng.c
@@ -3,69 +3,43 @@
 //  License :  https://github.com/randombit/botan/blob/master/doc/license.txt
 
 #include "cckprng.h"
+#include "yarrow/yarrow.h"
 
 int cckprng_init(cckprng_ctx_t ctx, size_t nbytes, const void *seed) {
-	// Nothing to do here, rdrand cannot be seeded.
-	return CCKPRNG_OK;
+	prngInitialize(&ctx->prng);
+	ctx->bytes_generated = ctx->bytes_since_entropy = 0;
+
+	return cckprng_reseed(ctx, nbytes, seed);
 }
 
 int cckprng_reseed(cckprng_ctx_t ctx, size_t nbytes, const void *seed) {
-	// Nothing to do here, rdrand cannot be seeded.
+	prngInput(ctx->prng, (BYTE *)seed, (UINT)nbytes, 0, 0);
+	prngAllowReseed(ctx->prng, 5000);
+	ctx->bytes_since_entropy = 0;
 	return CCKPRNG_OK;
 }
 
 int cckprng_addentropy(cckprng_ctx_t ctx, size_t nbytes, const void *seed) {
-	// Nothing to do here, rdrand cannot be seeded.
+	prngInput(ctx->prng, (BYTE *)seed, (UINT)nbytes, 0, 0);
+	ctx->bytes_since_entropy = 0;
 	return CCKPRNG_OK;
 }
 
-// MARK: -
-
-static int rdrand(uint64_t *value) {
-	if (value == NULL) return CCKPRNG_ABORT;
-
-	// According to Intel, RDRAND is guaranteed to generate a random
-	// number within 10 retries on a working CPU.
-	const size_t retries = 10;
-	for (size_t i = 0; i < retries; i++) {
-		uint64_t output = 0;
-		int cf = 0;
-
-		asm("rdrand %0; adcl $0, %1" : "=r" (output), "=r" (cf) : "0" (output), "1" (cf) : "cc");
-
-		if (cf == 1) {
-			*value = output;
-			return CCKPRNG_OK;
-		}
-	}
-
-	return CCKPRNG_ABORT;
-}
-
 int cckprng_generate(cckprng_ctx_t ctx, size_t nbytes, void *out) {
-	uint64_t random = 0;
-	uint8_t *buffer = (uint8_t *)out;
+	BYTE *buffer = (BYTE *)out;
+	while (nbytes > UINT32_MAX) {
+		prngOutput(ctx->prng, buffer, UINT32_MAX);
+		ctx->bytes_generated += UINT32_MAX;
+		ctx->bytes_since_entropy += UINT32_MAX;
 
-	for (size_t i = 0; i < nbytes; i++) {
-		int mod8 = i % 8;
-		if (mod8 == 0) {
-			int error = rdrand(&random);
-			if (error != CCKPRNG_OK) return error;
-		}
-
-		uint8_t byte;
-		if (mod8 == 0) byte = random & 0xFF;
-		else if (mod8 == 1) byte = (random >> 8) & 0xFF;
-		else if (mod8 == 2) byte = (random >> 16) & 0xFF;
-		else if (mod8 == 3) byte = (random >> 24) & 0xFF;
-		else if (mod8 == 4) byte = (random >> 32) & 0xFF;
-		else if (mod8 == 5) byte = (random >> 40) & 0xFF;
-		else if (mod8 == 6) byte = (random >> 48) & 0xFF;
-		else if (mod8 == 7) byte = (random >> 56) & 0xFF;
-		else return CCKPRNG_ABORT;
-
-		buffer[i] = byte;
+		buffer += UINT32_MAX;
+		nbytes -= UINT32_MAX;
+		nbytes = CC_MAX(nbytes, 0);
 	}
+
+	prngOutput(ctx->prng, buffer, (UINT)nbytes);
+	ctx->bytes_generated += nbytes;
+	ctx->bytes_since_entropy += nbytes;
 
 	return CCKPRNG_OK;
 }
