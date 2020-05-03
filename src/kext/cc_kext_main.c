8ca775a85bad832569314aa3b0d421552cf5f06e
William Kent <wjk011@gmail.com>
2020-05-02 22:29:34 -0400

    Specify a cckprng_ctx as well

--- src/kext/cc_kext_main.c
+++ src/kext/cc_kext_main.c
@@ -3,6 +3,7 @@
 #include "register_crypto.h"
 #include "prng_random.h"
 #include <corecrypto/cckprng.h>
+#include "../algorithms/yarrow/yarrow.h"
 
 extern struct crypto_functions pdcrypto_internal_functions;
 
@@ -14,13 +15,24 @@ static const struct cckprng_funcs cc_kprng_fns = {
 	.generate = cckprng_generate
 };
 
+static struct cckprng_ctx cc_kprng_ctx = {
+	.prng = NULL,
+	.bytes_since_entropy = 0,
+	.bytes_generated = 0
+};
+
 kern_return_t cc_kext_start(kmod_info_t * ki, void *d)
 {
 	int ret = register_crypto_functions(&pdcrypto_internal_functions);
 	if (ret == -1) {
 		printf("warning: corecrypto could not be registered. Did another crypto handler beat us to it?\n");
 	} else {
-		register_and_init_prng(NULL, &cc_kprng_fns);
+		prng_error_status error = prngInitialize(&cc_kprng_ctx.prng);
+		if (error != PRNG_SUCCESS) {
+			panic("prngInitialize() failed with code %d", error);
+		}
+
+		register_and_init_prng(&cc_kprng_ctx, &cc_kprng_fns);
 		printf("corecrypto loaded\n");
 	}
 
