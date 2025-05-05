2b7c61179a033163656148557a9e14125f092af3
Zormeister <57213163+Zormeister@users.noreply.github.com>
2025-05-05 18:47:34 +1000

    AES: Implement selector for CBC mode
    Signed-off-by: Zormeister <57213163+Zormeister@users.noreply.github.com>

--- src/aes/selector.c
+++ src/aes/selector.c
@@ -9,10 +9,14 @@
 #include <corecrypto/ccmode_impl.h>
 #include <corecrypto/cc_runtime_config.h>
 
+extern const struct ccmode_cbc pdcaes_cbc_encrypt;
+extern const struct ccmode_cbc pdcaes_cbc_decrypt;
 extern const struct ccmode_ecb pdcaes_ecb_encrypt;
 extern const struct ccmode_ecb pdcaes_ecb_decrypt;
 
 #if CC_LINUX_ASM
+extern const struct ccmode_cbc ccaes_intel_cbc_encrypt_aesni_mode;
+extern const struct ccmode_cbc ccaes_intel_cbc_decrypt_aesni_mode;
 extern const struct ccmode_ecb ccaes_intel_ecb_encrypt_aesni_mode;
 extern const struct ccmode_ecb ccaes_intel_ecb_decrypt_aesni_mode;
 #endif
@@ -41,4 +45,30 @@ const struct ccmode_ecb *ccaes_ecb_decrypt_mode(void) {
 #endif
 
     return &pdcaes_ecb_decrypt;
+};
+
+const struct ccmode_cbc *ccaes_cbc_encrypt_mode(void) {
+
+#if CCAES_INTEL_ASM
+    if (CC_HAS_AESNI() && CC_HAS_AVX2()) {
+        return &ccaes_intel_cbc_encrypt_aesni_mode;
+    }
+#elif CC_LINUX_ASM
+    return &ccaes_intel_cbc_encrypt_aesni_mode;
+#endif
+
+    return &pdcaes_cbc_encrypt;
+};
+
+const struct ccmode_cbc *ccaes_cbc_decrypt_mode(void) {
+
+#if CCAES_INTEL_ASM
+    if (CC_HAS_AESNI() && CC_HAS_AVX2()) {
+        return &ccaes_intel_cbc_decrypt_aesni_mode;
+    }
+#elif CC_LINUX_ASM
+    return &ccaes_intel_cbc_decrypt_aesni_mode;
+#endif
+
+    return &pdcaes_cbc_decrypt;
 };
