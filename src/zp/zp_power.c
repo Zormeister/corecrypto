020121952168749b85e550bf71b6d02e3d363561
Zormeister <57213163+Zormeister@users.noreply.github.com>
2025-05-10 17:28:32 +1000

    ZP: WIP code upload
    Signed-off-by: Zormeister <57213163+Zormeister@users.noreply.github.com>

---
+++ src/zp/zp_power.c
@@ -0,0 +1,12 @@
+//
+//  zp_power.c
+//  corecrypto
+//
+//  Created by Zormeister on 6/5/2025.
+//
+
+#include <corecrypto/cczp.h>
+
+int cczp_power(cczp_const_t zp, cc_unit *r, const cc_unit *m, const cc_unit *e) {
+    return CCERR_OK;
+}
