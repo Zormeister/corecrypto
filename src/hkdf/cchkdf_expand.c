e06d6be52df016201b627e0945df52d613e5159d
Zormeister <57213163+Zormeister@users.noreply.github.com>
2025-05-10 17:26:47 +1000

    HKDF: WIP code upload
    Signed-off-by: Zormeister <57213163+Zormeister@users.noreply.github.com>

---
+++ src/hkdf/cchkdf_expand.c
@@ -0,0 +1,16 @@
+//
+//  cchkdf_expand.c
+//  corecrypto
+//
+//  Created by Zormeister on 7/5/2025.
+//
+
+#include <corecrypto/cchkdf.h>
+#include <corecrypto/cchmac.h>
+
+int cchkdf_expand(struct ccdigest_info *di, size_t prk_len, const void *prk,
+                  size_t info_len, const void *info,
+                  size_t derived_len,  void *derived_key) {
+    /* TODO */
+    return CCERR_OK;
+}
---
+++ src/hkdf/cchkdf_extract.c
@@ -0,0 +1,20 @@
+//
+//  cchkdf_extract.c
+//  corecrypto
+//
+//  Created by Zormeister on 7/5/2025.
+//
+
+#include <corecrypto/cchkdf.h>
+#include <corecrypto/cchmac.h>
+#include <string.h>
+
+int cchkdf_extract(struct ccdigest_info *di, size_t salt_len, const void *salt, size_t ikm_len, const void *ikm, void *prk) {
+    if (salt == NULL) {
+        uint8_t zero_salt[16] = {0};
+        cchmac(di, 16, zero_salt, ikm_len, ikm, prk);
+    } else {
+        cchmac(di, salt_len, salt, ikm_len, ikm, prk);
+    }
+    return CCERR_OK;
+}
