875763113ee943109aaa9f4e8dae820eda50342d
Zormeister <57213163+Zormeister@users.noreply.github.com>
2025-05-10 17:26:25 +1000

    Dummy: Remove more garbage
    Signed-off-by: Zormeister <57213163+Zormeister@users.noreply.github.com>

--- src/dummy/pdcrypto_dummy.c
+++ src/dummy/pdcrypto_dummy.c
@@ -26,65 +26,10 @@ static int pdcmode_cbc_init_dummy(const struct ccmode_cbc *cbc, cccbc_ctx *ctx,
 	return -1;
 }
 
-const struct ccmode_ecb pdcaes_ecb_encrypt_dummy = {
-    .init = pdcmode_ecb_init_dummy
-};
-
-const struct ccmode_ecb pdcaes_ecb_decrypt_dummy = {
-    .init = pdcmode_ecb_init_dummy
-};
-
-static int pdcmode_aes_cbc_init_dummy(const struct ccmode_cbc *cbc, cccbc_ctx *ctx,
-                                       size_t key_len, const void *key)
-{
-    printf("%s\n", __func__);
-	return -1;
-}
-
-const struct ccmode_cbc pdcaes_cbc_encrypt_dummy = {
-    .init = pdcmode_aes_cbc_init_dummy
-};
-
-const struct ccmode_cbc pdcaes_cbc_decrypt_dummy = {
-    .init = pdcmode_aes_cbc_init_dummy
-};
-
 const struct ccmode_xts pdcaes_xts_encrypt_dummy;
 const struct ccmode_xts pdcaes_xts_decrypt_dummy;
 const struct ccmode_gcm pdcaes_gcm_encrypt_dummy;
 const struct ccmode_gcm pdcaes_gcm_decrypt_dummy;
-
-const struct ccmode_ecb pdcdes_ecb_encrypt_dummy = {
-    .init = pdcmode_ecb_init_dummy
-};
-
-const struct ccmode_ecb pdcdes_ecb_decrypt_dummy = {
-    .init = pdcmode_ecb_init_dummy
-};
-
-const struct ccmode_cbc pdcdes_cbc_encrypt_dummy = {
-    .init = pdcmode_cbc_init_dummy
-};
-
-const struct ccmode_cbc pdcdes_cbc_decrypt_dummy = {
-    .init = pdcmode_cbc_init_dummy
-};
-
-const struct ccmode_ecb pdctdes_ecb_encrypt_dummy = {
-    .init = pdcmode_ecb_init_dummy
-};
-
-const struct ccmode_ecb pdctdes_ecb_decrypt_dummy = {
-    .init = pdcmode_ecb_init_dummy
-};
-
-const struct ccmode_cbc pdctdes_cbc_encrypt_dummy = {
-    .init = pdcmode_cbc_init_dummy
-};
-
-const struct ccmode_cbc pdctdes_cbc_decrypt_dummy = {
-    .init = pdcmode_cbc_init_dummy
-};
 
 const struct ccrc4_info pdcrc4_info_dummy;
 
@@ -105,13 +50,6 @@ const struct ccmode_ecb pdccast_ecb_decrypt_dummy = {
     .init = pdcmode_ecb_init_dummy
 };
 
-
-int pdcdes_key_is_weak_fn_dummy(void *key,
-                                unsigned long  length)
-{
-    printf("%s\n", __func__);
-    return -1;
-}
 
 void pdcdes_key_set_odd_parity_fn_dummy(void *key,
                                         unsigned long length)
