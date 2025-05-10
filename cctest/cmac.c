9f2528ec04cfc81913887bfbb46df506fce7bfaa
Zormeister <57213163+Zormeister@users.noreply.github.com>
2025-05-10 17:32:59 +1000

    Test(CMAC): Comment out for now
    Signed-off-by: Zormeister <57213163+Zormeister@users.noreply.github.com>

--- cctest/cmac.c
+++ cctest/cmac.c
@@ -12,7 +12,7 @@
 static const uint8_t key[] = {0x2b,0x7e,0x15,0x16,0x28,0xae,0xd2,0xa6,0xab,0xf7,0x15,0x88,0x09,0xcf,0x4f,0x3c};
 
 void TestCMAC() {
-    cccmac_mode_decl(ccaes_cbc_encrypt_mode(), cmac);
-    
-    cccmac_init(ccaes_cbc_encrypt_mode(), cmac, CCAES_KEY_SIZE_128, key);
+    //cccmac_mode_decl(ccaes_cbc_encrypt_mode(), cmac);
+
+    //cccmac_init(ccaes_cbc_encrypt_mode(), cmac, CCAES_KEY_SIZE_128, key);
 }
