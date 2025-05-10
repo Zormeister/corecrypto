403abe8afb3cf785cd2cdfbd082154d05a3cbc13
Zormeister <57213163+Zormeister@users.noreply.github.com>
2025-05-10 15:21:21 +1000

    DES: Add in the mode selectors for ECB and CBC

---
+++ src/des/ccdes_modes.c
@@ -0,0 +1,24 @@
+//
+//  ccdes_modes.c
+//  corecrypto
+//
+//  Created by Zormeister on 10/5/2025.
+//
+
+#include <corecrypto/ccmode_factory.h>
+#include <corecrypto/ccdes.h>
+
+CCMODE_CBC_FACTORY(des, encrypt);
+CCMODE_CBC_FACTORY(des, decrypt);
+
+extern const struct ccmode_ecb ccdes_ltc_ecb_encrypt_mode;
+extern const struct ccmode_ecb ccdes_ltc_ecb_decrypt_mode;
+
+
+const struct ccmode_ecb *ccdes_ecb_decrypt_mode(void) {
+    return &ccdes_ltc_ecb_encrypt_mode;
+}
+
+const struct ccmode_ecb *ccdes_ecb_encrypt_mode(void) {
+    return &ccdes_ltc_ecb_decrypt_mode;
+}
