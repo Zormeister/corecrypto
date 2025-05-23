ce6ec11aa4ccba06038df32633eec87472473ef3
Zormeister <57213163+Zormeister@users.noreply.github.com>
2025-05-23 11:03:48 +1000

    BigNum: implement ccz_is_zero
    Signed-off-by: Zormeister <57213163+Zormeister@users.noreply.github.com>

---
+++ src/ccz/ccz_is_zero.c
@@ -0,0 +1,13 @@
+//
+//  ccz_is_zero.c
+//  corecrypto
+//
+//  Created by Zormeister on 23/5/2025.
+//
+
+#include <corecrypto/ccz.h>
+#include <corecrypto/ccn.h>
+
+bool ccz_is_zero(ccz *z) {
+    return ccn_is_zero(z->n, z->num);
+}
