980440577403662c84dddeea3b534ef28df97802
Zormeister <57213163+Zormeister@users.noreply.github.com>
2025-06-13 20:05:33 +1000

    AES: print if AES-NI is in use
    Signed-off-by: Zormeister <57213163+Zormeister@users.noreply.github.com>

--- src/aes/selector.c
+++ src/aes/selector.c
@@ -5,6 +5,7 @@
 //  Created by Zormeister on 4/5/2025.
 //
 
+#include <corecrypto/cc_debug.h>
 #include <corecrypto/ccaes.h>
 #include <corecrypto/ccmode_impl.h>
 #include <corecrypto/cc_runtime_config.h>
@@ -20,6 +21,9 @@ const struct ccmode_ecb *ccaes_ecb_encrypt_mode(void) {
 
 #if CCAES_INTEL_ASM
     if (CC_HAS_AESNI()) {
+#if CORECRYPTO_DEBUG
+        cc_printf("corecrypto(aes): using AES-NI for ECB encrypt\n");
+#endif
         return &ccaes_intel_ecb_encrypt_aesni_mode;
     }
 #endif
@@ -31,6 +35,9 @@ const struct ccmode_ecb *ccaes_ecb_decrypt_mode(void) {
 
 #if CCAES_INTEL_ASM
     if (CC_HAS_AESNI()) {
+#if CORECRYPTO_DEBUG
+        cc_printf("corecrypto(aes): using AES-NI for ECB decrypt\n");
+#endif
         return &ccaes_intel_ecb_decrypt_aesni_mode;
     }
 #endif
@@ -42,6 +49,9 @@ const struct ccmode_cbc *ccaes_cbc_encrypt_mode(void) {
 
 #if CCAES_INTEL_ASM
     if (CC_HAS_AESNI()) {
+#if CORECRYPTO_DEBUG
+        cc_printf("corecrypto(aes): using AES-NI for CBC encrypt\n");
+#endif
         return &ccaes_intel_cbc_encrypt_aesni_mode;
     }
 #endif
@@ -53,6 +63,9 @@ const struct ccmode_cbc *ccaes_cbc_decrypt_mode(void) {
 
 #if CCAES_INTEL_ASM
     if (CC_HAS_AESNI()) {
+#if CORECRYPTO_DEBUG
+        cc_printf("corecrypto(aes): using AES-NI for CBC decrypt\n");
+#endif
         return &ccaes_intel_cbc_decrypt_aesni_mode;
     }
 #endif
