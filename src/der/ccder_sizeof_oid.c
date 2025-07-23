d95d74a30d46a72658214c5178a97cf0cc02bdd9
Zormeister <57213163+Zormeister@users.noreply.github.com>
2025-07-23 21:22:37 +1000

    der: implement ccder_sizeof_oid
    Signed-off-by: Zormeister <57213163+Zormeister@users.noreply.github.com>

---
+++ src/der/ccder_sizeof_oid.c
@@ -0,0 +1,12 @@
+//
+//  ccder_sizeof_oid.c
+//  corecrypto
+//
+//  Created by Zormeister on 23/7/2025.
+//
+
+#include <corecrypto/ccder.h>
+
+size_t ccder_sizeof_oid(ccoid_t oid) {
+    return ccoid_size(oid);
+}
